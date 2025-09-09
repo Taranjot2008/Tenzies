e13bc97e1170296628bbbb18650f545f904f2fd8
Taranjot Singh <taranjotscience@gmail.com>
2025-09-08 23:17:29 -0300

    updated gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <Router>
+    <App />
+  </Router>
 )
