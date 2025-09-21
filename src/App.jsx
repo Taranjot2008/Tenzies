4c51fdc9e8508a2c05cb51fdc744ebc6f3b83c98
Taranjot Singh <taranjotscience@gmail.com>
2025-09-20 21:19:15 -0300

    Update with Dark Mode, Fixed Moves

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRef, useEffect } from "react"
+import { useEffect, useRef } from 'react'
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 import Header from "./components/header"
@@ -8,6 +8,7 @@ import Die from './components/Die'
 export default function App() {
 
   const [dices, setDices] = React.useState(() => generateallNewDice())
+  const [count, setCount] = React.useState(20)
   const buttonRef = useRef(null)
 
   const gameWon = dices.every(die => die.isHeld) &&
@@ -32,14 +33,17 @@ export default function App() {
 
   function rollDice() {
 
-    if (!gameWon){
+    if (!gameWon && count > 0) {
 
       setDices(oldDice => oldDice.map(die => 
         die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
       ))
     } else {
       setDices(generateallNewDice())
+      setCount(21)
     }
+
+    setCount(prevCount => prevCount - 1)
 
   }
 
@@ -65,6 +69,21 @@ export default function App() {
         {gameWon && <p>Congratulations! You Won! Click "New Game" to start new game</p>}
       </div>}
       
+      {count === 0 && !gameWon && <>
+        <div aria-live='polite' className="countStatus w-lg h-[240px] absolute top-6 z-10 bg-white
+        text-2xl text-black rounded-xl shadow-[2px_2px_8px_#00000033] text-center px-5 pt-5
+        max-sm:w-3xs max-sm:h-[220px] max-sm:text-lg">
+          <p className='retry-text text-red-500 font-medium'>🧨Last move was a dud. Game over? Click New Game to start a new game.</p>
+          <button ref={buttonRef} className='reset-button mt-5 bg-[#fb6f92] rounded-lg p-3 text-[20px] transition:transform duration-150
+                        font-semibold hover:cursor-pointer hover:-translate-y-1 max-sm:text-[16px]' onClick={rollDice}>
+            <span className='text-white'>New Game</span>
+          </button>
+        </div>
+        <div className="w-full h-full bg-gray-700/50 absolute top-0 z-5"></div>
+      </>
+      
+      }
+
 
       <Header />
 
@@ -76,6 +95,13 @@ export default function App() {
                         font-semibold hover:cursor-pointer hover:-translate-y-1 max-sm:text-[18px]' onClick={rollDice}>
         {gameWon ? 'New Game' : 'Roll Dice'}
       </button>
+
+      <div className="countDiv bg-[#d8e2dc] mt-16 px-3 py-10 rounded-lg shadow-[2px_2px_8px_#00000033] text-center
+      max-sm:w-10/12 max-sm:py-5 md:w-9/12 lg:w-2/5">
+        <p className='countText text-[#4a5759] text-5xl font-bold max-sm:text-3xl'>
+          <i className="fa-solid fa-dice mr-4"></i>
+          Rolls left: {count}</p>
+      </div>
     </main>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,44 @@
 import '../index.css'
+import { useState, useEffect, use } from 'react'
 
 export default function Header() {
+
+
+    const [darkState, setDarkState] = useState(false)
+
+    useEffect(() => {
+        const htmlElement = document.documentElement
+        if (darkState) {
+            htmlElement.classList.add('dark')
+        }
+        else {
+            htmlElement.classList.remove('dark')
+        }
+    }, [darkState])
+
+    console.log(darkState)
+
+
     return (
         <section className="header-section flex flex-col items-center gap-[20px]
         mb-8">
             <h1 className="heading1 text-[#4a5759] md:text-7xl font-bold
-            max-sm:text-5xl">TENZIES</h1>
+            max-sm:text-5xl dark:text-white">TENZIES</h1>
             <p className='heading-text md:text-xl max-sm:text-lg text-[#5c3a2e] font-medium
-            max-sm:text-center max-[340px]:text-[12px]'>Roll all dice until all dice are same. Start a new game once you are finished</p>
+            max-sm:text-center max-sm:px-2 max-[340px]:text-[12px] lg:text-2xl dark:text-white'>Roll all dice until all dice are same. Start a new game once you are finished</p>
+
+            <div className="theme-switcher absolute top-10 right-10 text-3xl flex gap-4">
+                {darkState ? <span className='w-10 h-10 p-4 rounded-full bg-white flex items-center justify-center'>
+                    <i className="fa-solid fa-moon hover:cursor-pointer text-[#495057] transition duration-300"
+                    onClick={() => setDarkState(prev => !prev)}
+                    aria-label="Switch to light theme"></i>
+                </span> : 
+                <span className='w-10 h-10 p-4 rounded-full bg-white flex items-center justify-center'>    
+                    <i className="fa-solid fa-sun hover:cursor-pointer text-[#ffc300] transition duration-300"
+                    onClick={() => setDarkState(prev => !prev)}
+                    aria-label="Switch to dark theme"></i>
+                </span>}
+            </div>
         </section>
     )
 }
